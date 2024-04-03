284d5071f48a55e23ab86b29de69370e4d974356
Emmanuella Ikeoluwapo Omolade <51097137+Ikeoluwapo@users.noreply.github.com>
2024-04-03 17:56:59 +0100

    Update RESUME.py

--- RESUME.py
+++ RESUME.py
@@ -14,16 +14,15 @@ nlp = spacy.load('en_core_web_sm')
 
 
 #libraries to parse the resume pdf files
-from pyresparser import ResumeParser   #esumeParser class from the pyresparser module.
+from pyresparser import ResumeParser   # ResumeParser class from the pyresparser module.
 from pdfminer3.layout import LAParams, LTTextBox 
 from pdfminer3.pdfpage import PDFPage
 from pdfminer3.pdfinterp import PDFResourceManager
 from pdfminer3.pdfinterp import PDFPageInterpreter
 from pdfminer3.converter import TextConverter
 import io, random 
-from streamlit_tags import st_tags
 from PIL import Image
-import pymysql 
+# import pymysql 
 # import pafy
 import plotly.express as px
 
@@ -32,7 +31,7 @@ st.set_page_config(
     page_title="AI CV Job Recommendation"
 )
 
-# connecting to database 
+# Function to read PDF files
 def pdf_reader(file):
     res_manager = PDFResourceManager()
     file_handler = io.StringIO()
@@ -58,35 +57,28 @@ def start():
     if pdf_file is not None:
         with st.spinner("Uploading your CV ..."):
             time.sleep(4)
-    save_image_path = "./Resume/Uploaded_Resume/" + pdf_file.name
-    if hasattr(pdf_file, 'name'):
-        with open(save_image_path, "wb") as f:
-            f.write(pdf_file.getbuffer())
-    else:
-        st.error("Uploaded file has no name attribute.")
-else:
-    st.error("No file was uploaded.")
+        save_image_path = "./Resume/Uploaded_Resume/" + pdf_file.name
+        if hasattr(pdf_file, 'name'):
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+        else:
+            st.error("Uploaded file has no name attribute.")
         
-            #getting data from cv
-        cv_data= ResumeParser(save_image_path).get_extracted_data()
+        # Getting data from the CV
+        cv_data = ResumeParser(save_image_path).get_extracted_data()
         if cv_data:
             cv_content = pdf_reader(save_image_path)
 
             st.header("CV Analysis")
-            st.success("Hello" + cv_data["name"])
+            st.success("Hello " + cv_data["name"])
             
             try:
-                # st.success("Hello "+ cv_data["name"])
                 st.text(cv_data["mobile_number"])
-                # st.text(cv_data["degree"])
-                # st.text(cv_data["experience"])
                 st.text(cv_data["skills"])
-                # st.text(cv_data["total_experience"])
-                # st.text(cv_data["no_of_pages"])
             except:
                 pass
 
-            experience_level = 0 # initialization
+            experience_level = 0
             if cv_data["no_of_pages"] == 1:
                 experience_level = "Fresher"
             elif cv_data["no_of_pages"] == 2:
@@ -98,39 +90,39 @@ else:
             
             st.text("You fall under category of someone who is " + experience_level)
 
-        # recommend career
-        #keywords = st_tags(label = "You current skills are")
-        for i in cv_data["skills"]:
-            if i.lower() in ["Coding", "Python", "mySQL", "Programming", "html", "css", "Javascript", "Software"]:
-                st.text(i.lower())
-                st.text("Recommended job: Data Science, Software development, Frontend development, backend")
+            # Recommend career
+            for i in cv_data["skills"]:
+                if i.lower() in ["coding", "python", "mysql", "programming", "html", "css", "javascript", "software"]:
+                    st.text(i.lower())
+                    st.text("Recommended job: Data Science, Software development, Frontend development, Backend")
 
-            elif i.lower() in ["Engineering", "Construction", "Architect", "Architecture", "Building"]:
+                elif i.lower() in ["Engineering", "Construction", "Architect", "Architecture", "Building"]:
                 print(i.lower())
                 st.text("Recommended job: Architecture, Civil Engineering")
 
-            elif i.lower() in ["Care giving", "Health", "Bioinformatics", "Caring"]:
-                print(i.lower())
-                st.text("Recommended job: Medicine, Nursing, Microbiology")
-
-            elif i.lower() in ["Law, Justice, Freedom"]:
-                print(i.lower())
-                st.text("Recommended job: Law, Political science")
-
-            elif i.lower() in ["Traveling", "Hospitality", "scheduling"]:
-                print(i.lower())
-                st.text("Recommended job: Hotel management, Aviation")
-            elif i.lower() in ["Meeting people", "Sales", "warehouse", "Marketing"]:
-                print(i.lower())
-                st.text("Recommended job: sales, marketing, entreprenuer, business")
-            elif i.lower() in ["Entertainment", "Music", "Sport", "Art"]:
-                print(i.lower())
-                st.text("Recommended job: Music, sport analyst, coaching, comedy, acting")
-            elif i.lower() in ["Finance", "Numbers", "Data", "Yrchnical", ""]:
-                print(i.lower())
-                st.text("Recommended job: Accounting, data analyst, financial analyst")
-                
-
+                elif i.lower() in ["Care giving", "Health", "Bioinformatics", "Caring"]:
+                    print(i.lower())
+                    st.text("Recommended job: Medicine, Nursing, Microbiology")
+    
+                elif i.lower() in ["Law, Justice, Freedom"]:
+                    print(i.lower())
+                    st.text("Recommended job: Law, Political science")
+    
+                elif i.lower() in ["Traveling", "Hospitality", "scheduling"]:
+                    print(i.lower())
+                    st.text("Recommended job: Hotel management, Aviation")
+                elif i.lower() in ["Meeting people", "Sales", "warehouse", "Marketing"]:
+                    print(i.lower())
+                    st.text("Recommended job: sales, marketing, entreprenuer, business")
+                elif i.lower() in ["Entertainment", "Music", "Sport", "Art"]:
+                    print(i.lower())
+                    st.text("Recommended job: Music, sport analyst, coaching, comedy, acting")
+                elif i.lower() in ["Finance", "Numbers", "Data", "Yrchnical", ""]:
+                    print(i.lower())
+                    st.text("Recommended job: Accounting, data analyst, financial analyst")
+                    
+    else:
+        st.error("No file was uploaded.")
 
 start()
 
